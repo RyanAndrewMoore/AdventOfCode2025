e3bab151c5181fd673ffe7264d4c26ca62db0e63
Ryan Andrew Moore <67835212+RyanAndrewMoore@users.noreply.github.com>
2026-01-07 17:04:54 -0800

    Solve part 2?

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -1,6 +1,68 @@
-﻿using Day9;
+﻿using System.Drawing;
+using SkiaSharp;
+using Day9;
+using Rectangle = Day9.Rectangle;
+
+var bmp = new SKBitmap(3000, 3000);
+using SKCanvas canvas = new(bmp);
 
 var part1Tiles = new TileMap(args[0]);
 var largestArea = part1Tiles.GetAllRectangleAreas().Max();
 
 Console.WriteLine($"Part 1 Answer: {largestArea}");
+
+var part2Tiles = new TileMap(args[0]);
+
+List<(long Area, Tile TileA, Tile TileB)> allAreasWithTiles = part2Tiles.GetAllRectangleAreasWithTiles();
+
+allAreasWithTiles.Sort(((areaA, areaB) => areaA.Area.CompareTo(areaB.Area)));
+allAreasWithTiles.Reverse();
+
+var largestValidArea = 0L;
+
+var valid = true;
+
+foreach (var area in allAreasWithTiles)
+{
+    var rectangle = new Rectangle(area.TileA, area.TileB);
+
+    foreach (var line in rectangle.BoundaryLines)
+    {
+        if (part2Tiles.DoesLineCrossBoundary(line)) valid = false;
+    }
+
+    if (valid)
+    {
+        largestValidArea = area.Area;
+        break;
+    }
+
+    valid = true;
+}
+
+foreach (var tile in part2Tiles._map)
+{
+    canvas.DrawCircle(new SKPoint((float)tile.X / 35, (float)tile.Y / 35), 5f, new SKPaint
+    {
+        Color = new SKColor((uint)Color.Red.ToArgb())
+    });
+}
+
+foreach (var line in part2Tiles._boundary)
+{
+    canvas.DrawLine(
+        line.Start.X / 35f,
+        line.Start.Y / 35f,
+        line.End.X / 35f,
+        line.End.Y / 35f,
+        new SKPaint
+        {
+            Color = new SKColor((uint)Color.Green.ToArgb()),
+            StrokeWidth = 5
+        }
+    );
+}
+
+// SKFileWStream fs = new("/home/rmoore/RiderProjects/AdventOfCode/AdventOfCode2025/Day9/input/canvas.jpg");
+
+// bmp.Encode(fs, SKEncodedImageFormat.Jpeg, quality: 50);
---
+++ Day9TwoElectricBoogaloo/Program.cs
@@ -0,0 +1,79 @@
+﻿using Clipper2Lib;
+
+var filePath = args[0];
+
+var redTiles = File.ReadAllLines(filePath)
+    .SelectMany(s => s.Split(',')).Select(int.Parse).ToArray();
+
+var tilePoly = new Paths64() { Clipper.MakePath(redTiles) };
+
+
+var pointList = tilePoly.SelectMany(path64 => path64)
+    .ToList();
+
+var cornerPairs = pointList.SelectMany((_, i) =>
+        pointList.Skip(i + 1), (point1, point2) => new
+    {
+        C1 = point1,
+        C2 = point2
+    }
+);
+
+var rectangles = cornerPairs.Select(pair =>
+    new Rect64(
+        l: Math.Min(pair.C1.X, pair.C2.X),
+        t: Math.Min(pair.C1.Y, pair.C2.Y),
+        r: Math.Max(pair.C1.X, pair.C2.X),
+        b: Math.Max(pair.C1.Y, pair.C2.Y)
+    )
+).ToList();
+
+var rectPaths = rectangles.Select(rect => new
+        {
+            RealRectangle = new Paths64()
+            {
+                Clipper.MakePath([
+                    rect.left, rect.top,
+                    rect.right, rect.top,
+                    rect.right, rect.bottom,
+                    rect.left, rect.bottom
+                ])
+            },
+
+            BlockyRectangle = new Paths64()
+            {
+                Clipper.MakePath([
+                    rect.left, rect.top,
+                    rect.right + 1, rect.top,
+                    rect.right + 1, rect.bottom + 1,
+                    rect.left, rect.bottom + 1
+                ])
+            }
+        }
+    ).Select(arg => new
+        {
+            arg.RealRectangle,
+            arg.BlockyRectangle,
+            RealArea = Clipper.Area(arg.RealRectangle),
+            BlockyArea = Clipper.Area(arg.BlockyRectangle)
+        }
+    )
+    .ToList();
+
+
+rectPaths.Sort((x, y) =>
+    x.BlockyArea <= y.BlockyArea ? 1 : -1
+);
+
+var largestArea = 0L;
+foreach (var tup in rectPaths)
+{
+    var intersect = Clipper.Intersect(tilePoly, tup.RealRectangle, FillRule.EvenOdd);
+    if ((long) tup.RealArea == (long)Clipper.Area(intersect))
+    {
+        largestArea = (long) tup.BlockyArea;
+        break;
+    }
+}
+
+Console.WriteLine($"{largestArea}");
